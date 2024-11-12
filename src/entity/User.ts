4710ae9714ff7a078addaf24b315cfc91afd105c
Kelvin Silvestre <kelvinsc7@gmail.com>
2024-11-12 15:19:16 -0300

    :books: docs: docstrings creation for entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,20 +1,32 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-//TODO Crie a entidade de User
+
+// Entidade que representa um usuário.
 @Entity()
 export class User {
+
+  // ID do usuário.
   @PrimaryGeneratedColumn()
   id!: number;
 
+  // Primeiro nome do usuário.
   @Column({ length: 100 })
   firstName: string;
 
+  // Último nome do usuário.
   @Column({ length: 100 })
   lastName: string;
 
+  // E-mail do usuário (único).
   @Column({ length: 100, unique: true })
   email: string;
 
+  /**
+   * Construtor da entidade User.
+   * firstName: Primeiro nome do usuário.
+   * lastName: Último nome do usuário.
+   * email: E-mail do usuário.
+   */
   constructor(firstName: string, lastName: string, email: string) {
     this.firstName = firstName;
     this.lastName = lastName;
