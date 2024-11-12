55ec386f9bc88942cdd57cfc2e4d727bf64c73e3
Kelvin Silvestre <kelvinsc7@gmail.com>
2024-11-12 15:12:38 -0300

    :books: docs: docstrings creation for services

--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -2,17 +2,27 @@ import { postRepository } from '../repository/post.repository';
 import { PostRequestDTO } from '../controller/dto/post-request.dto';
 import { PostResponseDTO } from '../controller/dto/post-response.dto';
 
+
+// Serviço responsável por gerenciar operações de post.
 export class PostService {
+
+  /**
+   * Cria um novo post com os dados fornecidos.
+   * É passados os parâmetros do post a ser criado.
+   * Retorna o post criado.
+   */
   async createPost(data: PostRequestDTO): Promise<PostResponseDTO> {
     const post = postRepository().create(data);
     await postRepository().save(post);
     return post;
   }
 
+  // Retorna uma lista de todos os posts.
   async getPosts(): Promise<PostResponseDTO[]> {
     return postRepository().find();
   }
 
+  // Retorna um post por ID.
   async getPostById(id: number): Promise<PostResponseDTO | null> {
     return postRepository().findOneBy({ id });
   }
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,17 +2,27 @@ import { userRepository } from '../repository/user.repository';
 import { UserRequestDTO } from '../controller/dto/user-request.dto';
 import { UserResponseDTO } from '../controller/dto/user-response.dto';
 
+
+// Serviço responsável por gerenciar operações de usuário.
 export class UserService {
+
+  /**
+   * Cria um novo usuário com os dados fornecidos.
+   * É passados os parâmetros do usuário a ser criado.
+   * Retorna o usuário criado.
+   */
   async createUser(data: UserRequestDTO): Promise<UserResponseDTO> {
     const user = userRepository().create(data);
     await userRepository().save(user);
     return user;
   }
 
+  // Retorna uma lista de todos os usuários.
   async getUsers(): Promise<UserResponseDTO[]> {
     return userRepository().find();
   }
 
+  // Retorna um usuário por ID.
   async getUserById(id: number): Promise<UserResponseDTO | null> {
     return userRepository().findOneBy({ id });
   }
